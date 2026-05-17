67814e5f464a9a90d8f2eb959152cdd4ce302f21
hashimt-950 <hashimt950@gmail.com>
2026-05-17 17:45:39 +0500

    Add user model

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,28 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db.js");
+const User = require("./models/Users.model.js");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Stermile is running</h1>");
+});
+
+app.post("/api/register", async (req, res) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    await user.save();
+    console.log("user created");
+    res.json(user);
+  } catch (error) {
+    console.log("error while registering user: ", error.message);
+  }
 });
 
 connectDB();
