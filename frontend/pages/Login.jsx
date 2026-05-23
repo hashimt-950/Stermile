f5645281085b01fa05a33b9e96e29a4ac3fcdcc1
hashimt-950 <hashimt950@gmail.com>
2026-05-23 14:58:15 +0500

    add login funcitonality to log in user, add protected routes to allow access if user is logged in

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -1,10 +1,56 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const login = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+
+      const data = await response.json();
+      localStorage.setItem("token", data.token);
+      navigate("/");
+      console.log(data);
+    } catch (error) {
+      console.log("error occured while logging in: ", error.message);
+    }
+  };
+
   return (
     <>
-      <input type="email" placeholder="Enter email" />
-      <input type="password" placeholder="Enter password" />
+      <input
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        value={email}
+        type="email"
+        placeholder="Enter email"
+      />
+
+      <input
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        value={password}
+        type="password"
+        placeholder="Enter password"
+      />
+
+      <button onClick={login}>Login</button>
 
       <h6>
         <Link to="/signup">Create an account</Link>
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
         </Routes>
